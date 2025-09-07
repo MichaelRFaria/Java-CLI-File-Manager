0577963d30a0d78497c6e7ea14f261ffb3a5f365
Michael Faria <michaelronaldfaria@gmail.com>
2025-09-07 18:01:06 +0100

    Fixed UserInput.java to correctly display error messages upon incorrect user inputs.

--- src/UserInput.java
+++ src/UserInput.java
@@ -10,30 +10,43 @@ public class UserInput {
         when opening/running files, this should say "Please input a valid file name" (should not say folder)
      */
     public static String getFileNameInput() {
-        System.out.println(System.lineSeparator().repeat(50)); // clears console in a way that is not environment-dependent
-        System.out.println("Please input a valid file/folder name: ");
 
         while (true) {
+            System.out.println(System.lineSeparator().repeat(50)); // clears console in a way that is not environment-dependent
+            System.out.println("Please input a valid file/folder name: ");
+
             String input = Main.getScanner().next();
+            // scanner doesnt except empty inputs this is stupid until actual name rules checking rules are implemented
             if (!input.isEmpty()) { // name checking rules go here
                 return input;
             } else {
-                System.out.println("File name input is invalid. Please try again: ");
+                System.out.println("File name input is invalid. Please try again.");
+                try {
+                    Thread.sleep(1500);
+                } catch (InterruptedException ex) {
+                    throw new RuntimeException(ex);
+                }
             }
         }
     }
 
     public static String getAbsolutePathInput() {
-        System.out.println(System.lineSeparator().repeat(50)); // clears console in a way that is not environment-dependent
-        System.out.println("Please input a valid absolute path: ");
 
         while (true) {
+            System.out.println(System.lineSeparator().repeat(50)); // clears console in a way that is not environment-dependent
+            System.out.println("Please input a valid absolute path: ");
+
             String input = Main.getScanner().next();
             File temp = new File(input);
             if (temp.isAbsolute()) { // path checking rules go here
                 return input;
             } else {
-                System.out.println("File path is entered incorrectly. Please try again: ");
+                System.out.println("File path is entered incorrectly. Please try again.");
+                try {
+                    Thread.sleep(1500);
+                } catch (InterruptedException ex) {
+                    throw new RuntimeException(ex);
+                }
             }
         }
     }

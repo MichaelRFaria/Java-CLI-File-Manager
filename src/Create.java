7a024d92084bf7500a4ebbd3b2e22b3abc2d7329
Michael Faria <michaelronaldfaria@gmail.com>
2025-09-27 14:37:39 +0100

    Improved Create.java documentation by adding Javadoc comments and additional single/multi-line comments.

--- src/Create.java
+++ src/Create.java
@@ -2,12 +2,23 @@ import java.io.File;
 import java.io.IOException;
 
 public class Create {
+    /**
+     * The method to create a file.
+     *
+     * @param fileName the name of the file/folder you want to create, either given by a relative file path (lone file name),
+     *                 or an absolute file path, differentiated by the {@code isAbsolute} parameter.
+     * @param isAbsolute whether you have inputted a relative file path (relative to the program's default directory "output") or an absolute file path.
+     * @return Whether the file was successfully created or not (either already exists, or operation error).
+     */
     public static boolean createFile(String fileName, boolean isAbsolute) {
         String fullFileName;
 
+        // if-conditional to figure out if we need to prepend the default directory path
         if (isAbsolute) {
+            // if isAbsolute is true, then we already have the full file path
             fullFileName = fileName;
         } else {
+            // if isAbsolute is false, then we must be working in the program's default directory "output", so we prepend the default directory path
             fullFileName = Main.getDefaultPath() + fileName;
         }
 
@@ -17,9 +28,14 @@ public class Create {
             boolean created;
             File file = new File(fullFileName);
 
-            if (fullFileName.contains(".")) { // if there is a ',' then we must be creating a file
+            /* this program assumes if folders don't have a '.' and files have a '.' (the file extension)
+            * figuring out if we are creating a file or a folder */
+
+            // if there is a ',' then we must be creating a file
+            if (fullFileName.contains(".")) {
                 created = file.createNewFile();
-            } else { // if there is no '.' then we must be creating a folder
+            // if there is no '.' then we must be creating a folder
+            } else {
                 created = file.mkdir();
             }
 
@@ -34,7 +50,6 @@ public class Create {
             Main.delay();
 
             return created;
-
         } catch (IOException e) {
             System.out.println("An error occurred:");
             System.out.println(e.getMessage()+"\n");
